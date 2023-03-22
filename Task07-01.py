18ecb35bf0a879d142ad9822cb5ed6c784ad77e2
ice4869 <128584407+ice4869@users.noreply.github.com>
2023-03-22 16:41:03 +0800

    Task07-01.py
    請問下方程式碼會回傳什麼結果？

---
+++ Task07-01.py
@@ -0,0 +1,15 @@
+#a = list(range(6))
+#while a:
+#    a.pop()
+#    print(len(a))
+
+#a = 10
+#while a:
+#    a = a - 1
+#    print(a)
+
+x = 6
+y = 5
+z = 7
+
+print((x > y )and (y > z) or (y - z % 2 > x) or (z + x*6 > y**2))
